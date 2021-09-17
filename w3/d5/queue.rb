d7ce663beeec181064657359856ca500f72c8feb
pamajama <pamela.palomita@gmail.com>
2021-09-17 15:02:49 -0700

    Added skeleton for polytree project

---
+++ w3/d5/queue.rb
@@ -0,0 +1,18 @@
+class Queue
+  attr_reader :q
+  def initialize
+    @q = []
+  end
+
+  def enqueue(el)
+    @q.push(el)
+  end
+
+  def dequeue
+    @q.shift
+  end
+
+  def peek
+    @q[0]
+  end
+end
