d4bc652570782f06dde5fdd2b3e65f70d228dac3
pamajama <pamela.palomita@gmail.com>
2021-09-20 17:09:49 -0700

    update moves

--- w3/d5/polytree-node/lib/knight_pathfinder.rb
+++ w3/d5/polytree-node/lib/knight_pathfinder.rb
@@ -1,5 +1,6 @@
 class KnightPathFinder
   attr_reader :root_node
+  KNIGHT_MOVES = [[1,2], [2,1], [-1,2], [2,-1], [-2,1], [1,-2], [-1,-2], [-2,-1]]
 
   def initialize(position)
     @root_node = PolyTreeNode.new(position)
@@ -8,8 +9,16 @@ class KnightPathFinder
 
   def build_move_tree
   end 
+  
+  def self.valid_moves(position) #if [2,2] this should return an array of length 8, that hass all the sums of @@knight moves added to positon
+    return false if @considered_positions.include?(position)
 
-  def self.valid_moves(position)
-    
   end 
+
+  def new_move_positions(position)
+    return false if !KnightPathFinder.valid_moves(position) #return an array
+    @considered_positions << position
+
+  end
+
 end 
